bbec0363cc0bf6c8c3644448909eca0ae0c9b383
Kcbread <95199268+Kcbread@users.noreply.github.com>
2022-01-08 14:09:18 +0800

    test

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,11 @@
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+		int a = 15;
+		if (a == 15){
+			System.out.println("success");
+		
+		}
+}
+}
